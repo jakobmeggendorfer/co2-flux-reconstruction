b22a7a762b263e4744314a00441062e8ba4df415
Sweety Mohanty <33155955+swemoh@users.noreply.github.com>
2025-01-20 09:25:33 +0100

    Update ocean_basins.py

--- notebooks/ocean_basins.py
+++ notebooks/ocean_basins.py
@@ -1,6 +1,9 @@
 import pandas as pd
 
-def get_zoned(appended_data):
+def get_zoned_df(appended_data):
+    '''
+    returns multiple dataframes corresponding to 4 different basins
+    '''
     
     zone_ARCTIC = appended_data.loc[appended_data['nav_lat'] > 70.0]
     zone_ARCTIC['zone'] = 'ARCTIC'
@@ -19,3 +22,21 @@ def get_zoned(appended_data):
     zone_SOUTHERN_OCEAN['zone'] = 'SOUTHERN_OCEAN'
     
     return zone_ARCTIC, zone_NORTH_ATLANTIC, zone_EQ_PACIFIC, zone_SOUTHERN_OCEAN
+
+def assign_basins(nav_lat, nav_lon):
+    '''
+    assigns basins to individual latitude and longitude values. Better use as a lambda function.
+    '''
+    if nav_lat > 70.0:
+        return 'ARCTIC'
+    elif -75.0 <= nav_lon <= 0.0 and 10 <= nav_lat <= 70: 
+        return 'NORTH_ATLANTIC'
+    elif -10.0 <= nav_lat <= 10.0:
+        if 105.0 <= nav_lon <= 180.0 or -180.0 <= nav_lon <= -80.0:
+            return 'EQ_PACIFIC'
+    elif nav_lat <= -45:
+        return 'SOUTHERN_OCEAN'
+    else:
+        return 'OTHER'
+
+data_df['basin_name'] = data_df.apply(lambda row: assign_basins(row['nav_lat'], row['nav_lon']), axis=1)
